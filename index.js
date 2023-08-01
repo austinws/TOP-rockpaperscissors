9db04df6d449324e8359ff09a859c0e94b10487a
austinws <austinwsmith9317@gmail.com>
2023-08-01 14:26:16 -0500

    99% done, now theres just a type script error that needs to be fixed.

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-const options = ["stein", "papir", "saks"];
+export{};
+
+const choices = ["stein", "papir", "saks"];
 
 function getComputerChoice (){
   const choice = options[Math.floor(Math.random()*options.length)];
@@ -34,6 +36,44 @@ function playRound(playerSelection, computerSelection){
   }
 }
 
-const playerSelection = "stein";
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+function getPlayerChoice(){
+  let validatedInput = false;
+  while(validatedInput == false){
+    const choice = ["stein, papir, eller saks"];
+    if(choice == null){
+      continue;
+    }
+    const choiceInLower = choice.toLowerCase();
+    if(options.includes(choiceInLower)){
+      validatedInput == true;
+      return choiceInLower;
+      
+    }
+  }
+}
+
+function game (){
+  let scorePlayer = 0;
+  let scoreComputer = 0;
+  for (let i = 0, i < 5, i++){
+    const playerSelection = getPlayerChoice();
+    const computerSelection = getComputerChoice();
+    console.log(playRound(playerSelection, computerSelection));
+    if (checkWinner(playerSelection, computerSelection) == "Player"){
+      scorePlayer ++;
+    }
+    else if (checkWinner(playerSelection, computerSelection) == "Computer"){
+      scoreComputer ++;
+    }
+  }
+  console.log("The game is over.")
+  if(scorePlayer > scoreComputer){
+    console.log("The humans win this time.")
+  }
+  else if(scorePlayer < scoreComputer){
+    console.log("Computers win, you human meatbags.")
+  }
+  else {
+    console.log("Somehow its a tie!")
+  }
+}
